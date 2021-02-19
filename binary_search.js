0d5d92fd63b1c7b4a1c97d6e63f58d648a176324
Savchhenko <savchenko@phys.vsu.ru>
2021-02-19 12:09:13 +0300

    new Array(1000)

--- binary_search.js
+++ binary_search.js
@@ -1,4 +1,9 @@
-const arr = [1,2,3,4,5,6,7,8,9];
+// const arr = [1,2,3,4,5,6,7,8,9];
+const arr = new Array(1000);
+for(let i = 0; i < arr.length; i++) {
+    arr[i] = i + 1;
+}
+
 const el = Math.floor(Math.random() * arr.length);
 console.log('Searchable element: ', el);
 
