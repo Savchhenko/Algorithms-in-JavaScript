e53afd7005a520928d2508238b47873d492c02a7
Savchhenko <savchenko@phys.vsu.ru>
2021-02-19 13:18:27 +0300

    func that counts the frequency of elem

--- binary_search.js
+++ binary_search.js
@@ -1,4 +1,3 @@
-// const arr = [1,2,3,4,5,6,7,8,9];
 const arr = new Array(1000);
 for(let i = 0; i < arr.length; i++) {
     arr[i] = i + 1;
@@ -11,10 +10,8 @@ function binarySearchElement (arr, el) {
     // O(log N) - base of the logarithm is 2
     let left = -1;
     let rigth = arr.length;
-
     while (rigth - left > 1) {
         const mid = Math.floor((left + rigth) / 2);
-
         if (arr[mid] === el) {
             return mid;
         }
@@ -28,3 +25,28 @@ function binarySearchElement (arr, el) {
 }
 
 console.log('Index of the element in array: ', binarySearchElement(arr, el));
+
+
+const arr2 = [1,5,7,4,3,5,1,9,10,12,11,16,2,5,7,8,6];
+
+// the sorting method works with elements as with a string, so we will define a custom function
+arr2.sort((a, b) => a - b); // array was sorted
+
+console.log(countFreq(arr2, 5));
+
+//function that counts the frequency of occurrence of a particular element
+function countFreq(arr, el) {
+    const posEl = binarySearchElement(arr, el); //element's position in array
+    if (posEl === -1) {
+        return 0; // frequency = 0
+    }
+    let i = posEl;
+    while (arr[i] === el) {
+        i--;
+    }
+    let j = posEl;
+    while (arr[j] === el) {
+        j++;
+    }
+    return j - i - 1;
+}
